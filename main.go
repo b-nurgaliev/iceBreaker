ff5e9f71a2f5eb35dc4b039cd04c5dbf200e6581
bnurgaliev <xeroiiin@gmail.com>
2025-01-26 23:06:23 +0100

    added new struct and methods

--- main.go
+++ main.go
@@ -33,15 +33,22 @@ func main() {
 	}
 	// migration (creating User table)
 	db.AutoMigrate(&User{})
+	db.AutoMigrate(&Company{})
 
 	// init Gin
 	r := gin.Default()
 
-	// routes
+	// routes for user
 	r.POST("/users", createUser)
 	r.GET("/users/:id", getUser)
 	r.PUT("/users/:id", updateUser)
 	r.DELETE("/users/:id", deleteUser)
+
+	// routes for company
+	r.POST("/companies", createCompany)
+	r.GET("/companies/:id", getCompany)
+	r.PUT("/companies/:id", updateCompany)
+	r.DELETE("/companies/:id", deleteCompany)
 
 	// server run
 	r.Run(":8080")
@@ -95,4 +102,53 @@ func deleteUser(c *gin.Context) {
 	}
 	db.Delete(&user)
 	c.JSON(http.StatusOK, gin.H{"message": "User deleted"})
+}
+
+// get company
+func getCompany(c *gin.Context) {
+	id := c.Param("id")
+	var company Company
+	if err := db.First(&company, id).Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Company not found"})
+		return
+	}
+	c.JSON(http.StatusOK, company)
+}
+
+// create company
+func createCompany(c *gin.Context) {
+	var company Company
+	if err := c.ShouldBindJSON(&company); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+	}
+	db.Create(&company)
+	c.JSON(http.StatusOK, company)
+}
+
+// update company
+func updateCompany(c *gin.Context) {
+	id := c.Param("id")
+	var company Company
+	if err := db.First(&company, id).Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Company not found"})
+		return
+	}
+	if err := c.ShouldBindJSON(&company); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	db.Save(&company)
+	c.JSON(http.StatusOK, company)
+}
+
+// delete user
+func deleteCompany(c *gin.Context) {
+	id := c.Param("id")
+	var company Company
+	if err := db.First(&company, id).Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Company not found"})
+		return
+	}
+	db.Delete(&company)
+	c.JSON(http.StatusOK, gin.H{"message": "Company deleted"})
 }
